664ccf3e5a821fa22da495855aaa673250f83489
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-02-22 14:25:43 +0530

    2nd Commit - SignIn Form

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import './App.css';
+import SignInForm from './SignInForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SignInForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/SignInForm.js
@@ -0,0 +1,37 @@
+// SignInForm.js
+
+import React from 'react';
+import './SignInForm.css';
+import logoImage from '../src/logo.png';
+
+function SignInForm() {
+  return (
+    <div className="signin-form">
+      <img alt="logo" className="logo" src={logoImage} />
+      <div className="mainTitle">Sign in</div>
+      <div className="title">Sign in to</div>
+      <div className="subtitle">Graphers</div>
+      <div className="pharagraph">If you don’t have an account register<br></br>You can <b>Register here!</b></div>
+
+      <form>
+        <div className="input-group">
+          <label htmlFor="email"></label>
+          <input type="text" id="email" name="email" placeholder="Email or Username" />
+          <label htmlFor="password"></label> 
+          <input type="password" id="password" name="password" placeholder="Password" />
+          <div className="forgot-password">Forgot password?</div>
+          <button type="submit">Login</button>
+          <div className="continue-with">or continue with</div>
+        </div>
+      </form>
+
+      
+      
+      <div className="social-icons">
+        {/* You can add social icons here */}
+      </div>
+    </div>
+  );
+}
+
+export default SignInForm;
